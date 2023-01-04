852bae15c7919774282e94ca1d3e057ae2ce3f32
Sowji-3 <118727241+Sowji-3@users.noreply.github.com>
2023-01-04 22:50:42 +0530

    Update checkurls.sh

--- ChargeVision/checkurls.sh
+++ ChargeVision/checkurls.sh
@@ -1,8 +1,8 @@
 
 STDOUTFILE=".tempCurlStdOut"
 > $STDOUTFILE
-while read $url
+while read url
 do
     urlstatus=$(curl --max-time 5 --silent --write-out %{response_code} --output "$STDOUTFILE" "$url" )
-    echo " $url  $urlstatus" 
+    echo "$url  $urlstatus" 
 done
