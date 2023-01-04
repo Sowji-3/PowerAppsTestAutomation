a37286fd70cf4e563b164f47d3666c2f806de5b8
Sowji-3 <118727241+Sowji-3@users.noreply.github.com>
2023-01-05 00:22:34 +0530

    Update checkurls.sh

--- ChargeVision/checkurls.sh
+++ ChargeVision/checkurls.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
-while read LINE; do
-  curl -o /dev/null --silent --head --write-out "%{http_code} $LINE\n" "$LINE"
-done < url-list.txt
+FILE=$1
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    echo "$FILE does not exist."
+    exit 1
+fi
+while IFS="" read -r p || [ -n "$p" ]
+# while IFS= read -r p
+do
+  retcode=$(curl -s -L -o /dev/null -I -w "%{http_code}" $p)
+if [ $retcode -eq 200 ]; then
+        restext="Website found"
+else
+        restext="Website NOT found"
+fi
+  printf '%s \n' "$restext $p $retcode"
+  echo "$restext $p $retcode" >> urlstatus1.txt
+done < $1
