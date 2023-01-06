d7b981d4c35197407db17e1266521772d07ba4b2
Sowji-3 <118727241+Sowji-3@users.noreply.github.com>
2023-01-06 20:04:26 +0530

    Update checkurls.sh

--- ChargeVision/checkurls.sh
+++ ChargeVision/checkurls.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 while read url
 do
-  urlstatus=$(curl -s   /dev/null --silent  --write-out '%{http_code}' "$url" )
+  
+  urlstatus=$(curl -s -L -o /dev/null -I -w "%{http_code}" $url)
   echo "$url $urlstatus"
 done < $1
