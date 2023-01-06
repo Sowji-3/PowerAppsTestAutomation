516c80e53e2391038172584e2e659c591f63f61b
Sowji-3 <118727241+Sowji-3@users.noreply.github.com>
2023-01-06 23:36:56 +0530

    Update checkurls.sh

--- ChargeVision/checkurls.sh
+++ ChargeVision/checkurls.sh
@@ -3,8 +3,8 @@ while read url
 do
 #  urlstatus=$(curl -s -L -o /dev/null -I -w '%{http_code}' "$url")
   urlstatus=$(curl -s -L -I -o /dev/null --silent  --write-out '%{http_code}' "$url" )
-  if $urlstatus= 200;
-  then 
+  
+  if $urlstatus= 200; then 
     echo "url Exists : $url"
   else
     echo "url doesnot exist : $url"
